9bb3c47aab0702e1f42df1fe84674e772c51e208
SHIVAKHAMPARIA <dummy@gmail.com>
2026-04-14 08:50:33 +0000

    day 8

--- server/src/main/java/com/edutech/progressive/controller/ClinicController.java
+++ server/src/main/java/com/edutech/progressive/controller/ClinicController.java
@@ -1,37 +1,95 @@
 package com.edutech.progressive.controller;
 
-import org.springframework.http.ResponseEntity;
+import com.edutech.progressive.entity.Clinic;
+import com.edutech.progressive.service.impl.ClinicServiceImplJpa;
 
-import com.edutech.progressive.dao.PatientDAOImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/clinic")
 public class ClinicController {
-    public ResponseEntity<List<PatientDAOImpl>> getAllClinics() {
-        return null;
+    
+    private ClinicServiceImplJpa clinicServiceImplJpa;
+
+    @Autowired
+    public ClinicController(ClinicServiceImplJpa clinicServiceImplJpa) {
+        this.clinicServiceImplJpa = clinicServiceImplJpa;
     }
 
-    public ResponseEntity<PatientDAOImpl> getClinicById(int clinicId) {
-        return null;
+    @GetMapping
+    public ResponseEntity<List<Clinic>> getAllClinics() {
+        try {
+            return new ResponseEntity<List<Clinic>>(clinicServiceImplJpa.getAllClinics(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<Integer> addClinic(PatientDAOImpl clinic) {
-        return null;
+    @GetMapping("/{clinicId}")
+    public ResponseEntity<Clinic> getClinicById(@PathVariable int clinicId) {
+        try {
+            return new ResponseEntity<Clinic>(clinicServiceImplJpa.getClinicById(clinicId), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<Void> updateClinic(int clinicId, PatientDAOImpl clinic) {
-        return null;
+    @PostMapping
+    public ResponseEntity<Integer> addClinic(@RequestBody Clinic clinic) {
+        try {
+            return new ResponseEntity<Integer>(clinicServiceImplJpa.addClinic(clinic), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<Void> deleteClinic(int clinicId) {
-        return null;
+    @PutMapping("/{clinicId}")
+    public ResponseEntity<Void> updateClinic(@PathVariable int clinicId, @RequestBody Clinic clinic) {
+        try {
+            clinic.setClinicId(clinicId);
+            clinicServiceImplJpa.updateClinic(clinic);
+            return new ResponseEntity<Void>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<List<PatientDAOImpl>> getAllClinicByLocation(String location) {
-        return null;
+    @DeleteMapping("/{clinicId}")
+    public ResponseEntity<Void> deleteClinic(@PathVariable int clinicId) {
+        try {
+            clinicServiceImplJpa.deleteClinic(clinicId);
+            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public ResponseEntity<List<PatientDAOImpl>> getAllClinicByDoctorId(int doctorId) {
-        return null;
+    @GetMapping("/location/{location}")
+    public ResponseEntity<List<Clinic>> getAllClinicByLocation(@PathVariable String location) {
+        try {
+            return new ResponseEntity<List<Clinic>>(clinicServiceImplJpa.getAllClinicByLocation(location), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/doctor/{doctorId}")
+    public ResponseEntity<List<Clinic>> getAllClinicByDoctorId(@PathVariable int doctorId) {
+        try {
+            return new ResponseEntity<List<Clinic>>(clinicServiceImplJpa.getAllClinicByDoctorId(doctorId), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- server/src/main/java/com/edutech/progressive/service/impl/ClinicServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/ClinicServiceImplJpa.java
@@ -1,45 +1,57 @@
 package com.edutech.progressive.service.impl;
 
-import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edutech.progressive.entity.Clinic;
+import com.edutech.progressive.repository.ClinicRepository;
 import com.edutech.progressive.service.ClinicService;
-
 
 @Service
 public class ClinicServiceImplJpa implements ClinicService {
 
-    @Override
-    public List<Clinic> getAllClinics() throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllClinics'");
+    private ClinicRepository clinicRepository;
+
+    @Autowired
+    public ClinicServiceImplJpa(ClinicRepository clinicRepository){
+        this.clinicRepository = clinicRepository;
     }
 
     @Override
-    public Clinic getClinicById(int clinicId) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getClinicById'");
+    public List<Clinic> getAllClinics() throws Exception {
+        return clinicRepository.findAll();
     }
 
     @Override
-    public Integer addClinic(Clinic clinic) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addClinic'");
+    public Clinic getClinicById(int clinicId) throws Exception {
+        return clinicRepository.findByClinicId(clinicId);
     }
 
     @Override
-    public void updateClinic(Clinic clinic) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateClinic'");
+    public Integer addClinic(Clinic clinic) throws Exception {
+        return clinicRepository.save(clinic).getClinicId();
     }
 
     @Override
-    public void deleteClinic(int clinicId) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteClinic'");
+    public void updateClinic(Clinic clinic) throws Exception {
+        clinicRepository.save(clinic);
     }
 
+    @Override
+    public void deleteClinic(int clinicId) throws Exception {
+        clinicRepository.deleteById(clinicId);
+    }
+
+    @Override
+    public List<Clinic> getAllClinicByLocation(String location){
+        return clinicRepository.findAllByLocation(location);
+    }
+
+    @Override
+    public List<Clinic> getAllClinicByDoctorId(int doctorId){
+        return clinicRepository.findAllByDoctorId(doctorId);
+    }
 }
