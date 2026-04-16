bb73579dff378814c9e859161bf11170f30c40b6
SHIVAKHAMPARIA <dummy@gmail.com>
2026-04-16 14:59:14 +0000

    day 12

--- server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJpa.java
@@ -1,9 +1,9 @@
 package com.edutech.progressive.service.impl;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
+// import java.util.Optional;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
